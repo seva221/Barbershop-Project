46c8781cc0d345724a86fc903ee6a2e0352314ed
sevap <sevapan6@gmail.com>
2025-12-28 11:02:57 +0200

    Made correct user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,19 +1,30 @@
+// models/User.model.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, minlength: 2 },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
+
+    role: {
+      type: String,
+      enum: ["customer", "business"],
+      default: "customer",
+    },
+
+    businessId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Business",
+    },
   },
   { timestamps: true }
 );
 
 userSchema.methods.toJSON = function () {
-  const user = this.toObject();
-  delete user.passwordHash;
-  delete user._id
-  return user;
+  const obj = this.toObject();
+  delete obj.passwordHash;
+  return obj;
 };
 
 export default mongoose.model("User", userSchema);
