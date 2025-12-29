b021f2cf88b58409a2275609e53f50d94639aba4
sevap <sevapan6@gmail.com>
2025-12-29 17:54:12 +0200

    Added export to register schema

--- backend/validations/auth.schema.js
+++ backend/validations/auth.schema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const registerSchema = z.object({
+export const registerSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
   password: z
