fa1f69d87614dae31a6b627e5e767dbf69a72fcc
IdanKor51 <idankor06@gmail.com>
2026-01-16 21:06:47 +0200

    middleware

--- backend/routes/auth.business.routes.js
+++ backend/routes/auth.business.routes.js
@@ -1,5 +1,6 @@
 // routes/auth.business.routes.js
 import express from "express";
+import authMiddleware from "../middleware/auth.middleware.js"
 import {
   registerBusiness,
   loginBusiness,
@@ -7,7 +8,9 @@ import {
 
 const router = express.Router();
 
-router.post("/register", registerBusiness);
-router.post("/login", loginBusiness);
+// the middleware handles JWT confirmation
+// if none or incorrect -> reject (400, 401)
+router.post("/register",authMiddleware, registerBusiness);
+router.post("/login", authMiddleware, loginBusiness);
 
 export default router;
--- backend/routes/auth.user.routes.js
+++ backend/routes/auth.user.routes.js
@@ -1,11 +1,14 @@
 // routes/auth.user.routes.js
 import express from "express";
+import authMiddleware from "../middleware/auth.middleware.js"
+
 import { registerUser, loginUser } from "../controllers/auth.user.controller.js";
 
 const router = express.Router();
 
-// /api/auth/user/ [register | login]
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+// the middleware handles JWT confirmation
+// if none or incorrect -> reject (400, 401) 
+router.post("/register", authMiddleware, registerUser);
+router.post("/login", authMiddleware, loginUser);
 
 export default router;
